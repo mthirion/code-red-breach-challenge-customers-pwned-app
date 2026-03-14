62e607293a0e70c4b4f37967aa8074aa55342a49
Michael Thirion <mthirion@redhat.com>
2026-03-14 19:17:20 +0100

    Update CustomerResource.java
    Fix import

--- src/main/java/org/acme/customer/rest/CustomerResource.java
+++ src/main/java/org/acme/customer/rest/CustomerResource.java
@@ -14,6 +14,7 @@ import jakarta.ws.rs.core.Response.Status;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
